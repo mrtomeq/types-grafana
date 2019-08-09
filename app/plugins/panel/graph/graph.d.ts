40db7ad628572ccfcd748ac1ffb4b932cfe6e10c
mrtomeq <tomeqc@gmail.com>
2019-08-08 20:01:51 -0400

    graph related updates

--- app/plugins/panel/graph/graph.d.ts
+++ app/plugins/panel/graph/graph.d.ts
@@ -7,3 +7,24 @@ import 'jquery.flot.stackpercent';
 import 'jquery.flot.fillbelow';
 import 'jquery.flot.crosshair';
 import './jquery.flot.events';
+
+declare class GraphElement{
+	ctrl: any;
+	tooltip: any;
+	dashboard: any;
+	annotations: object[];
+	panel: any;
+	plot: any;
+	sortedSeries: any[];
+	data: any[];
+	panelWidth: number;
+	eventManager: any;
+	thresholdManager: any;
+	timeRegionManager: any;
+	legendElem: HTMLElement;
+
+	constructor(scope: any, elem: any, timeSrv: any);
+
+}
+
+export { GraphElement }
---
+++ app/plugins/panel/graph/histogram.d.ts
@@ -0,0 +1,7 @@
+export declare function convertToHistogramData(
+	data: any,
+	bucketSize: number,
+	hiddenSeries: any,
+	min: number,
+	max: number
+  ): any[];
